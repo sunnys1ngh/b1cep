72889f3b169c6745b1b6bb081a233ad56882c542
sunnys1ngh <sunnys1ngh@outlook.com>
2022-03-12 21:51:30 +0000

    update

--- pscripts/build.ps1
+++
@@ -1,8 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
-Connect-AzAccount -TenantId [-TenantId]
-New-AzResourceGroup -Name [-Name] -Location [-Location]
-
-
-
-
